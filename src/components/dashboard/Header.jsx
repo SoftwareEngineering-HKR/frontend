b04f8437e5cbc25b757045f6c92189b89dad27a2
Bianca Casetta <biancacasetta@gmail.com>
2026-04-01 16:50:42 +0200

    adapted Header for reuse

--- src/components/dashboard/Header.jsx
+++ src/components/dashboard/Header.jsx
@@ -2,9 +2,15 @@ import { Home, Users, LogOut } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "../common/Button";
 
-export default function Header(props) {
-  const onlineCount = props.devices.filter((d) => d.isOnline).length;
-  const offlineCount = props.devices.filter((d) => !d.isOnline).length;
+export default function Header({ devices, isAdmin, onLogout, title="Smart Home", subtitle, icon=<Home className="w-6 h-6 text-white" />, actions }) {
+  const onlineCount = devices?.filter((d) => d.isOnline).length ?? 0;
+  const offlineCount = devices?.filter((d) => !d.isOnline).length ?? 0;
+
+  // device count if in Overview, user count if in Admin Panel
+  if (!subtitle) {
+    subtitle = `${onlineCount} online · ${offlineCount} offline`;
+  }
+  
   const navigate = useNavigate();
 
   return (
@@ -14,30 +20,34 @@ export default function Header(props) {
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
               <div className="w-10 h-10 bg-indigo-600 rounded-lg flex items-center justify-center">
-                <Home className="w-6 h-6 text-white" />
+                {icon}
               </div>
               <div>
                 <h1 className="text-xl font-bold text-gray-900 dark:text-white">
-                  Smart Home
+                  {title}
                 </h1>
                 <p className="text-sm text-gray-600 dark:text-gray-400">
-                  {onlineCount} online · {offlineCount} offline
+                  {subtitle}
                 </p>
               </div>
             </div>
             <div className="flex items-center gap-3">
-              {props.isAdmin && (
-                <Button
-                  text="Manage Users"
-                  icon={<Users className="w-4 h-4"/>}
-                  onClick={() => navigate("/admin")}
-                  variant="ghost"
-                />
+              { actions ?? (
+                <>
+                  {isAdmin && (
+                    <Button
+                      text="Manage Users"
+                      icon={<Users className="w-4 h-4"/>}
+                      onClick={() => navigate("/admin")}
+                      variant="ghost"
+                    />
+                  )}
+                </>
               )}
               <Button
                 text="Log Out"
                 icon={<LogOut className="w-4 h-4"/>}
-                onClick={props.onLogout}
+                onClick={onLogout}
                 variant="ghost"
               />
             </div>
