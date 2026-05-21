5e883ccad3e9797cd5a9cf7268e036878073219e
martamoroni <marta.moroni0203@stud.hkr.se>
2026-05-21 20:25:36 +0200

    Add toggle for dark/light mode in header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#4F46E5', // Indigo-600
-        secondary: '#E0E7FF',
-        accent: '#4338ca',
+        primary: "#4F46E5", // Indigo-600
+        secondary: "#E0E7FF",
+        accent: "#4338ca",
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ["Inter", "sans-serif"],
       },
       animation: {
-        fadeIn: 'fadeIn 0.3s ease-in-out',
+        fadeIn: "fadeIn 0.3s ease-in-out",
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
       },
     },
   },
   plugins: [],
-}
+};
