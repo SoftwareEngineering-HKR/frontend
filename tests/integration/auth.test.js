282069c26d56de4d50d0cfa795a95cfe47a51931
Bianca Casetta <biancacasetta@gmail.com>
2026-05-22 18:14:57 +0200

    start integration tests - login and signup

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,63 @@
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import * as authService from "../../src/service/api";
+import { send, useWebSocket } from "../../src/hooks/useWebSocket";
+
+const HTTP_URL = "http://localhost:8081";
+
+describe("Authentication API", () => {
+
+  const testUser = {
+    username: "test",
+    password: "testtest",
+  }
+  let accessToken = null;
+
+  afterAll(async () => {
+    console.log("access token after all tests", accessToken);
+    if (!accessToken) return;
+
+    try {
+      const { send } = useWebSocket(true, accessToken);
+      await send.deleteUser(testUser.username);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+
+  describe("/signup", () => {
+
+    it("Successful Signup", async () => {
+      const res = await authService.auth("signup", testUser);
+      console.log(res);
+      expect(res.success).toBeTruthy();
+      expect(typeof res.accessToken).toBe("string");
+      expect(res.accessToken).toBeDefined();
+
+      
+    });
+  })
+
+  describe("/login", () => {
+
+    it("Invalid Credentials", async () => {
+      const res = await authService.auth("login", { username: "test", password: "123" });
+      console.log(res);
+      expect(res.success).toBeFalsy();
+      expect(typeof res.error).toBe("string");
+      expect(res.error).toBe("Invalid credentials")
+      expect(res.accessToken).toBeUndefined();
+    });
+
+    it("Successful Login", async () => {
+      const res = await authService.auth("login", testUser);
+      console.log(res);
+      expect(res.success).toBeTruthy();
+      expect(typeof res.accessToken).toBe("string");
+      expect(res.accessToken).toBeDefined();
+
+      accessToken = res.accessToken;
+    });
+
+  })
+
+});
