2e2f084c775053ddf9a8e0e8e8dc18024fea8f6f
Bianca Casetta <biancacasetta@gmail.com>
2026-05-24 14:49:33 +0200

    add 'get all device info' tests

--- tests/integration/device.test.js
+++ tests/integration/device.test.js
@@ -16,7 +16,7 @@ const TEST_USERS = {
 describe("Device-related WebSocket Messages", { sequential: true }, () => {
     let adminWS;
     let userWS;
-    let initialDevices;
+    let devices;
 
     // log in as admin to be able to perform all device management actions
     // create user to test that users are not allowed to perform device actions
@@ -24,9 +24,6 @@ describe("Device-related WebSocket Messages", { sequential: true }, () => {
         try {
             const adminRes = await authService.auth("login", TEST_USERS.admin);
             adminWS = createWsClient(adminRes.accessToken);
-            const res = adminWS.waitFor((m) => m.type === "initial devices");
-            initialDevices = res.payload.devices;
-            console.log(initialDevices);
 
             const userRes = await authService.auth("signup", TEST_USERS.user);
             userWS = createWsClient(userRes.accessToken);
@@ -46,5 +43,42 @@ describe("Device-related WebSocket Messages", { sequential: true }, () => {
 
         console.log(`[cleanup] ${res.payload.message}`);
         adminWS.close();
+    });
+
+    describe("Get All Device Info", { sequential: true }, () => {
+        it("success", async () => {
+            adminWS.send("get all device info");
+            const res = await adminWS.waitFor((m) => m.type === "device info");
+
+            expect(res.payload).toHaveProperty("devices");
+            expect(Array.isArray(res.payload.devices)).toBe(true);
+
+            for (const device of res.payload.devices) {
+                expect(device).toHaveProperty("id");
+                expect(device).toHaveProperty("ip");
+                expect(device).toHaveProperty("users");
+                expect(typeof device.id).toBe("string");
+                expect(typeof device.ip).toBe("string");
+                expect(Array.isArray(device.users)).toBe(true);
+            }
+
+            devices = res.payload.devices;
+            console.log(devices);
+        });
+
+        it("permission denied for users", async () => {
+            userWS.send("get all device info");
+            const res = await userWS.waitFor(
+                (m) => m.type === "action response",
+            );
+
+            expect(res).toMatchObject({
+                type: "action response",
+                payload: {
+                    statusCode: 403,
+                    message: "Permission denied!",
+                },
+            });
+        });
     });
 });
