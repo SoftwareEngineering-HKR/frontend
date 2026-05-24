75ff9158e7b015409a9be33530aed0be70e412f6
Bianca Casetta <biancacasetta@gmail.com>
2026-05-24 12:25:12 +0200

    fix indentation

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -3,69 +3,70 @@ import * as authService from "../../src/service/api";
 import { createWsClient } from "./wsHelper";
 
 const TEST_USERS = {
-  user: {
-    username: `test_user`,
-    password: "testtest",
-  }, 
-  admin: {
-    username: "admin",
-    password: "password",
-  }
-}
+    user: {
+        username: `test_user`,
+        password: "testtest",
+    },
+    admin: {
+        username: "admin",
+        password: "password",
+    },
+};
 
 describe("Authentication API", { sequential: true }, () => {
+    describe("/signup", { sequential: true }, () => {
+        it("Successful Signup", async () => {
+            const res = await authService.auth("signup", TEST_USERS.user);
 
-  describe("/signup", { sequential: true }, () => {
+            expect(res.success).toBeTruthy();
+            expect(typeof res.accessToken).toBe("string");
+            expect(res.accessToken).toBeDefined();
+        });
 
-    it("Successful Signup", async () => {
-      const res = await authService.auth("signup", TEST_USERS.user);
+        it("Attempt to sign up with existing username", async () => {
+            const res = await authService.auth("signup", TEST_USERS.user);
 
-      expect(res.success).toBeTruthy();
-      expect(typeof res.accessToken).toBe("string");
-      expect(res.accessToken).toBeDefined();
+            expect(res.success).toBeFalsy();
+            expect(typeof res.accessToken).toBe("undefined");
+            expect(res.accessToken).toBeUndefined();
+        });
     });
 
-    it("Attempt to sign up with existing username", async () => {
-      const res = await authService.auth("signup", TEST_USERS.user);
+    describe("/login", { sequential: true }, () => {
+        it("Invalid Credentials", async () => {
+            const res = await authService.auth("login", {
+                username: "test",
+                password: "123",
+            });
 
-      expect(res.success).toBeFalsy();
-      expect(typeof res.accessToken).toBe("undefined");
-      expect(res.accessToken).toBeUndefined();
+            expect(res.success).toBeFalsy();
+            expect(typeof res.error).toBe("string");
+            expect(res.error).toBe("Invalid credentials");
+            expect(res.accessToken).toBeUndefined();
+        });
+
+        it("Successful Login", async () => {
+            const res = await authService.auth("login", TEST_USERS.user);
+            expect(res.success).toBeTruthy();
+            expect(typeof res.accessToken).toBe("string");
+            expect(res.accessToken).toBeDefined();
+        });
     });
 
-  });
-
-  describe("/login", { sequential: true }, () => {
-    it("Invalid Credentials", async () => {
-      const res = await authService.auth("login", { username: "test", password: "123" });
-  
-      expect(res.success).toBeFalsy();
-      expect(typeof res.error).toBe("string");
-      expect(res.error).toBe("Invalid credentials")
-      expect(res.accessToken).toBeUndefined();
+    // log in as admin to delete test user at the end of tests
+    afterAll(async () => {
+        try {
+            let res = await authService.auth("login", TEST_USERS.admin);
+            const ws = createWsClient(res.accessToken);
+            const promise = ws.waitFor((m) => m.type === "action response");
+            await ws.send("delete user", { name: TEST_USERS.user.username });
+            res = await promise;
+            console.log(`[cleanup] ${res.payload.message}`);
+            ws.close();
+        } catch (err) {
+            console.warn(
+                `[cleanup] Could not delete test user: ${err.message}`,
+            );
+        }
     });
-
-    it("Successful Login", async () => {
-      const res = await authService.auth("login", TEST_USERS.user);
-      expect(res.success).toBeTruthy();
-      expect(typeof res.accessToken).toBe("string");
-      expect(res.accessToken).toBeDefined();
-    });
-  });
-  
-  // log in as admin to delete test user at the end of tests
-  afterAll(async () => {
-    try {
-      let res = await authService.auth("login", TEST_USERS.admin);
-      const ws = createWsClient(res.accessToken);
-      const promise = ws.waitFor((m) => m.type === "action response");
-      await ws.send("delete user", { name: TEST_USERS.user.username });
-      res = await promise;
-      console.log(`[cleanup] ${res.payload.message}`);
-      ws.close();
-    } catch (err) {
-      console.warn(`[cleanup] Could not delete test user: ${err.message}`);
-    }
-  });
-
 });
